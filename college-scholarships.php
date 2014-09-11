248b61c8702b3868df336982ef0acac6637a3c04
nitinsareen31 <nitinsareen31@gmail.com>
2014-09-11 07:47:36 +0530

    chnages to website pages

---
+++ college-scholarships.php
@@ -0,0 +1,216 @@
+  <?php include_once("includes/config.php"); ?>
+  <?php include_once("includes/header.php");?>
+  <section class="int-hero-image hero-img-two">
+        <div class="container">
+            <h3>COMMUNITY INVOLVEMENT</h3>
+        </div>
+    </section>
+    <!-- Start Main Page Content-->
+    <div id="root" class="pageWrapper int-page-wrapper">
+        <section class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 left-nav-section">
+                    <div class="left-nav">
+                        <ul>
+                            <li class="active"><a href="#">College Scholarships</a></li>
+                            <li><a href="#">Dental Health Educator</a></li>
+                            <li><a href="#">Community Services</a></li>
+                            <li><a href="#">Feldman Orthodontics’ Big Give</a></li>
+                        </ul>
+                    </div>
+                    <div class="appt-cta">
+                        <a class="req-appt" href="#">
+                            <span class="icon-mail"></span>
+                            <span>Request an<br /> EvaLuation</span>
+                        </a>
+                    </div>
+                    <div class="beige-span">
+                        <div class="loc-header"><h4>Visit Us</h4></div>
+                        <div class="north-tpa-main">
+                            <h5><a href="">North Tampa</a></h5>
+                            <a class="loc-link" href="#">
+                                <span></span>
+                            </a>
+                            <p>1773 West Fletcher Avenue <br />          
+                                Tampa, FL 33612<br />
+                                <a href="tel:+8139682483">(813) 968-2483 (BITE)</a>
+                            </p>
+                        </div>
+                        <div class="south-tpa-main">
+                            <h5><a href="">South Tampa</a></h5>
+                            <a class="loc-link" href="#">
+                                <span></span>
+                            </a>
+                            <p>3739 Neptune Street<br />
+                                Tampa, FL 33629<br />
+                                <a href="tel:+8132548005">(813) 254-8005</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12 main-page-content">
+                    <article id="college-scholarships">
+                    <!--intro and share -->
+                    <div class="row">
+                        <div class="col-lg-8 col-md-8 col-sm-7 col-xs-12">
+                        <h1 class="page-title">Feldman College Scholarships </h1>
+                        <h4>“What a Smile Means to Me”</h4>
+                        <h5>Feldman Orthodontics Scholarship Essay Competition</h5>
+                        <p>This year’s winning essays of the $1000 scholarship was awarded to two outstanding seniors: Ka'Oni Toussaint, a graduating senior from Hillsborough High School, and Lisa Ryzhikov, a graduating senior from Sickles High School.</p>
+                        <p>This year’s winning essays of the $1000 scholarship was awarded to two outstanding seniors: Ka'Oni Toussaint, a graduating senior from Hillsborough High School, and Lisa Ryzhikov, a graduating senior from Sickles High School.</p>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-5 col-xs-12">
+                        <div id="application">
+                            <div class="row"><div class="col-xs-12 bubble"></div></div>
+                            <div class="row"><div class="col-xs-12"><a href="" class="button">scholarship guidlines & APplication</a></div></div>
+                        </div>
+                    </div>
+                    </div>
+                    <!--intro and share ends -->
+                    <div class="row marg-top">
+                        <div class="col-xs-12 col-lg-12 col-md-12 col-sm-12">
+                            <h4>Scholarship Recipients</h4>
+                            <ul class="main">
+                                <!-- latest smiles -->
+                                <li>
+                                    <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+                                        <div class="flipper">
+                                            <!-- image goes here -->
+                                            <div class="front">
+                                                <img src="images/placeholder/demo3.png" />
+                                            </div>
+                                            <!-- description text -->
+                                            <div class="back">
+                                                <h2>August 31, 2012</h2>
+                                                <p>Sickles High School Read her winning essay</p>
+                                            </div>
+                                    </div> 
+                                        </div>           
+                                    <div class="row">
+                                        <div class="col-xs-12">
+                                        <h5>Fantastic New Smiles</h5>
+                                    </div>
+                                    </div>
+                                </li>
+                                <!-- latest smiles -->
+                                <!-- latest smiles -->
+                                <li>
+                                    <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+                                        <div class="flipper">
+                                            <!-- image goes here -->
+                                            <div class="front">
+                                                <img src="images/placeholder/demo3.png" />
+                                            </div>
+                                            <!-- description text -->
+                                            <div class="back">
+                                                <h2>August 31, 2012</h2>
+                                                <p>Sickles High School Read her winning essay</p>
+                                            </div>
+                                    </div> 
+                                        </div>           
+                                    <div class="row">
+                                        <div class="col-xs-12">
+                                        <h5>Fantastic New Smiles</h5>
+                                    </div>
+                                    </div>
+                                </li>
+                                <!-- latest smiles -->
+                                <!-- latest smiles -->
+                                <li>
+                                    <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+                                        <div class="flipper">
+                                            <!-- image goes here -->
+                                            <div class="front">
+                                                <img src="images/placeholder/demo3.png" />
+                                            </div>
+                                            <!-- description text -->
+                                            <div class="back">
+                                                <h2>August 31, 2012</h2>
+                                                <p>Sickles High School Read her winning essay</p>
+                                            </div>
+                                    </div> 
+                                        </div>           
+                                    <div class="row">
+                                        <div class="col-xs-12">
+                                        <h5>Fantastic New Smiles</h5>
+                                    </div>
+                                    </div>
+                                </li>
+                                <!-- latest smiles -->
+                                <!-- latest smiles -->
+                                <li>
+                                    <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+                                        <div class="flipper">
+                                            <!-- image goes here -->
+                                            <div class="front">
+                                                <img src="images/placeholder/demo3.png" />
+                                            </div>
+                                            <!-- description text -->
+                                            <div class="back">
+                                                <h2>August 31, 2012</h2>
+                                                <p>Sickles High School Read her winning essay</p>
+                                            </div>
+                                    </div> 
+                                        </div>           
+                                    <div class="row">
+                                        <div class="col-xs-12">
+                                        <h5>Fantastic New Smiles</h5>
+                                    </div>
+                                    </div>
+                                </li>
+                                <!-- latest smiles -->
+                                <!-- latest smiles -->
+                                <li>
+                                    <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+                                        <div class="flipper">
+                                            <!-- image goes here -->
+                                            <div class="front">
+                                                <img src="images/placeholder/demo3.png" />
+                                            </div>
+                                            <!-- description text -->
+                                            <div class="back">
+                                                <h2>August 31, 2012</h2>
+                                                <p>Sickles High School Read her winning essay</p>
+                                            </div>
+                                    </div> 
+                                        </div>           
+                                    <div class="row">
+                                        <div class="col-xs-12">
+                                        <h5>Fantastic New Smiles</h5>
+                                    </div>
+                                    </div>
+                                </li>
+                                <!-- latest smiles -->
+                                <!-- latest smiles -->
+                                <li>
+                                    <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+                                        <div class="flipper">
+                                            <!-- image goes here -->
+                                            <div class="front">
+                                                <img src="images/placeholder/demo3.png" />
+                                            </div>
+                                            <!-- description text -->
+                                            <div class="back">
+                                                <h2>August 31, 2012</h2>
+                                                <p>Sickles High School Read her winning essay</p>
+                                            </div>
+                                    </div> 
+                                        </div>           
+                                    <div class="row">
+                                        <div class="col-xs-12">
+                                        <h5>Fantastic New Smiles</h5>
+                                    </div>
+                                    </div>
+                                </li>
+                                <!-- latest smiles -->
+                                <a href="#" class="more-smiles">More <span class="icon-down-circled"></span></a>
+                            </ul>
+                        </div>
+                    </div>
+        </section>
+    </div><!-- End Main Page Content-->
+       
+        <!-- Push for Sticky Footer-->
+        <div id="root_footer"></div>
+    </div>
+    <?php include_once("includes/footer.php");?>
--- special-services.php
+++ special-services.php
@@ -21,35 +21,7 @@
             </article>
            </div>
            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 left-nav-section">
-            <div class="appt-cta">
-                        <a class="req-appt" href="#">
-                            <span class="icon-mail"></span>
-                            <span>Request an<br /> EvaLuation</span>
-                        </a>
-                    </div>
-                    <div class="beige-span">
-                        <div class="loc-header"><h4>Visit Us</h4></div>
-                        <div class="north-tpa-main">
-                            <h5><a href="">North Tampa</a></h5>
-                            <a class="loc-link" href="#">
-                                <span></span>
-                            </a>
-                            <p>1773 West Fletcher Avenue <br />          
-                                Tampa, FL 33612<br />
-                                <a href="tel:+8139682483">(813) 968-2483 (BITE)</a>
-                            </p>
-                        </div>
-                        <div class="south-tpa-main">
-                            <h5><a href="">South Tampa</a></h5>
-                            <a class="loc-link" href="#">
-                                <span></span>
-                            </a>
-                            <p>3739 Neptune Street<br />
-                                Tampa, FL 33629<br />
-                                <a href="tel:+8132548005">(813) 254-8005</a>
-                            </p>
-                        </div>
-                    </div>      
+            <?php include_once("includes/sidebar-left-address.php");?>
            </div>
             </div>
         </section>
