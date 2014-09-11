6c987056a7d40727415cfa01c2363f1e9dbd0c98
nitinsareen31 <nitinsareen31@gmail.com>
2014-09-12 02:55:14 +0530

    changes

---
+++ emergency-care.php
@@ -0,0 +1,102 @@
+<?php include_once("includes/config.php"); ?>
+<?php include_once("includes/header.php");?>
+<section class="int-hero-image hero-img-two">
+        <div class="container">
+            <h3>BICUSPID BLOG</h3>
+        </div>
+    </section>
+    <!-- Start Main Page Content-->
+    <div id="root" class="pageWrapper int-page-wrapper">
+        <section class="container">
+            <!--intro and share -->
+                    <div class="row">
+                        <div class="col-lg-9 col-md-8 col-sm-7 col-xs-12">
+                        <h1 class="page-title">What Our Patients Are Saying </h1>
+                        <p>Were you happy with your patient experience and/or new smile at Feldman Orthodontics? Please help us spread the word by leaving your testimonial on your preferred review site. It’s easy and simple to do. Fnd out how by clicking the button to the right!</p>
+                    </div>
+                    <div class="col-lg-3 col-md-4 col-sm-5 col-xs-12">
+                        <h2 style="margin-top:0; font-size:2rem;">Emergency Contact</h2>
+                        <div class="emergency-box">
+                            <span class="phone-nu"><span class="icon-ambulance"></span> (813) 695-3709</span>
+                            <span class="span-small">(Emergencies Only)</span>
+                        </div>
+                        <p style="font-size:1.5rem; margin-top:10px; color:#284a6e;">(Please use only in true orthodontic emergencies)</p>
+                    </div>
+                    </div>
+                    <!--intor and share ends -->
+        </section>
+        <section class="container">
+            <h3 class="span-section-header blue">Orthodontic Emergencies</h3>
+        </section>
+        <div class="blue-span footer-main">
+            <section class="container" id="emergency">
+                <!-- info graphic -->
+                <div class="row marg-top bg-care">
+                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 align-cen ">
+                        <img src="images/site-structure/infograhic-sore.png" class="img-resp" />
+                    </div>
+                    <div class="col-lg-7 col-md-7 col-sm-7 col-xs-12 col-lg-offset-1 col-md-offset-1 col-sm-offset-1">
+                        <div class="blue-box">
+                            <p>You may take acetaminophen or other non-aspirin pain relievers while you adjust to your new braces. A warm wash cloth, heating pad or warm salt water rinses may reduce the soreness in your jaws. </p>
+                        </div>
+                    </div>
+                </div>
+                <!-- info graphic ends-->
+                <!-- info graphic -->
+                <div class="row marg-top bg-care">
+                    <div class="col-lg-7 col-md-7 col-sm-7 col-xs-12 col-lg-offset-1 col-md-offset-1 col-sm-offset-1">
+                        <div class="blue-box">
+                            <p>Try moving the wire away from the irritated area with a cotton swab or eraser. If the wire will not move, try covering the end of it with a small piece of cotton or a small amount of wax. If the wire is painful, you can cut it with nail clippers or scissors that have been washed and sterilized in alcohol. </p>
+                        </div>
+                    </div>
+                     <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 align-cen ">
+                        <img src="images/site-structure/infograhic-irritation.png" class="img-resp" />
+                    </div>
+                </div>
+                <!-- info graphic ends-->
+                <!-- info graphic -->
+                <div class="row marg-top bg-care">
+                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 align-cen ">
+                        <img src="images/site-structure/infograhic-bracket.png" class="img-resp" />
+                    </div>
+                    <div class="col-lg-7 col-md-7 col-sm-7 col-xs-12 col-lg-offset-1 col-md-offset-1 col-sm-offset-1">
+                        <div class="blue-box">
+                            <p><a href="#">First, call our office</a> to see if the bracket needs to be replaced. If you have a situation where you must cut the wire or slide a bracket off the wire, you may use fingernail clippers that have been washed and sterilized in alcohol. Please call our office on the next business day to schedule a repair appointment.</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- info graphic ends-->
+                <!-- info graphic -->
+                <div class="row marg-top bg-care">
+                    <div class="col-lg-7 col-md-7 col-sm-7 col-xs-12 col-lg-offset-1 col-md-offset-1 col-sm-offset-1">
+                        <div class="blue-box">
+                            <p><a href="#">Call our office</a> to see if it needs to be replaced. Some patients may lose a separator during treatment, which is common, and may not need to be replaced. </p>
+                        </div>
+                    </div>
+                     <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 align-cen ">
+                        <img src="images/site-structure/infograhic-separator.png" class="img-resp" />
+                    </div>
+                </div>
+                <!-- info graphic ends-->
+            </section>
+
+        </div>
+        <div class="share-div">
+            <section class="container">
+                <ul>
+                <li>Share This Article:</li>
+                <li><a href="#" class="share"></a></li>
+                <li><a href="#" class="facebook"></a></li>
+                <li><a href="#" class="tweet"></a></li>
+                <li><a href="#" class="linkidn"></a></li>
+                <li><a href="#" class="google"></a></li>
+                </ul>
+            </section>
+        </div>
+    </div><!-- End Main Page Content-->
+       
+        <!-- Push for Sticky Footer-->
+        <div id="root_footer"></div>
+    </div>
+    <!-- Footer-->
+    <?php include_once("includes/footer.php");?>
